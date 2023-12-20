ebb7bfb6c99f04e4e0e0c9cb102653ef076487c6
Roberto Nóbrega <r_t_4@hotmail.com>
2023-12-20 17:09:35 -0300

    Commit 8 ( branch main ): Início dos estudos do App, na seção 4. Estrutura do Projeto, com Pequeno Formulário, Botões, Customizações e Arquitetura MVC.

--- Secao_4/App_1/app/src/main/java/devandroid/robertonobrega/applistacurso/model/Pessoa.java
+++ Secao_4/App_1/app/src/main/java/devandroid/robertonobrega/applistacurso/model/Pessoa.java
@@ -2,11 +2,54 @@ package devandroid.robertonobrega.applistacurso.model;
 
 // Aula 29, Seção 4: Paradigma de Programação Orientada a Objetos. Nesta aula, começa a ensinar a implementar
 // a classe Pessoa.
+// Aula 30, Seção 4: Paradigma de Programação Orientada a Objetos. Atributo/Método.
 public class Pessoa{
+
+    private String primeiroNome; // Atributo da classe Pessoa, de tipo String, modificador de acesso private,
+    // chamado primeiroNome. Atalho: Após o ponto e vírgula, segure a tecla Ctrl, seguido da tecla D. Irá
+    // realizar, na linha seguinte, uma cópia do conteúdo da linha atual.
+    private String sobreNome; // Atributo.
+    private String cursoDesejado, telefoneContato; // Atributos. OBS: Numa mesma linha, é possível declarar
+    // mais de um atributo de um mesmo tipo de dado.
 
     public Pessoa(){ /* Método Construtor da classe Pessoa. OBS: Não é preciso criálo, pois ele é
                         definido por 'default' (ele ficará 'invisível na classe Pessoa'). */
 
     }
 
+    // Abaixo, teremos os métodos set e get.  Atalho: Clique com o botão direito do mouse. Escolha a opção
+    // Generate. Para gerar os métodos set e get, escolha a opção Getter and Setter. Depois, marque o(s)
+    // atributo(s) para gerar os métodos correspondentes.
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;  // Na aula 31, da Seção 4, o prof chega a usar o Modo Debug desta linha.
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
 }
--- Secao_4/App_1/app/src/main/java/devandroid/robertonobrega/applistacurso/view/MainActivity.java
+++ Secao_4/App_1/app/src/main/java/devandroid/robertonobrega/applistacurso/view/MainActivity.java
@@ -9,6 +9,9 @@ import devandroid.robertonobrega.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
 
+    // OBS: As novas versões do AndroidStudio ( para o Sistema Operacional Linux ), de 2021 para
+    // diante, possuem o Java, de forma embutida.
+
     /*
         Aula 21, Seção 4: O prof. informa que a pasta ( onde está o arquivo MainActivity.java )
         será a que trabalharemos muito. As outras duas pastas ( dentro da pasta java ), trata-se
@@ -28,13 +31,14 @@ public class MainActivity extends AppCompatActivity {
         pelo Emulador.
 
         Aula 29, Seção 4: Paradigma de Programação Oriantada a Objetos.
-
+        Aula 30, Seção 4: Paradigma de Programação Oriantada a Objetos. Método e Atributo.
     */
 
     Pessoa pessoa; // Definindo um objeto da classe Pessoa. Até esse ponto, o objeto não está instanciado. Atalho: Após
     // inserir o nome da classe (no caso, Pessoa), segure a tecla Ctrl e em seguida tecle a tecla de Espaço em Branco. Re-
     // sultado: O AndroidStudio irá completar, automaticamente, o nome do objeto da classe.
-
+    Pessoa outraPessoa;
+    String dadosPessoa, dadosOutraPessoa;  // OBS: Objetos declarados na aula 32, da seção 4.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,5 +47,31 @@ public class MainActivity extends AppCompatActivity {
         // tela do XML ). OBS: Atalho: Ao selecionar o nome da tela do XML (nesse caso, activity_main),
         // ao segurar a tecla Ctrl , clique em seguida a tecla B. Vai abrir a tela do XML.
         pessoa = new Pessoa(); // Objeto pessoa foi instanciado.
+        pessoa.setPrimeiroNome("Roberto"); // Chamando o método setPrimeiroNome. Nele, estará preenchendo um valor para o atributo
+        // private, chamado primeiroNome.
+        pessoa.setSobreNome("Nóbrega");
+        pessoa.setCursoDesejado("Android Developer");
+        pessoa.setTelefoneContato("(83)9888-888");
+
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Luiz");
+        outraPessoa.setSobreNome("Alves");
+        outraPessoa.setCursoDesejado("Java");
+        outraPessoa.setTelefoneContato("(83)9888-876");
+        // int parada = 0; // Aula 31, Seção 4. Esta variável será uasada como 'parada', por meio do uso do Modo Debug.
+        // Para usar o modo Debug, escolha a linha de código, que será usada a ser analisada. Escolhida, aponte o cur-
+        // sor do mouse no número da linha e clique em cima do mesmo. Irá aparecer uma 'bolinha vermelha'. OBS: Para
+        // desmarcar, clique em cima da 'bolinha vermelha'.
+
+        dadosPessoa = "Primeiro Nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso Desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de Contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        int parada = 0;
     }
 }
